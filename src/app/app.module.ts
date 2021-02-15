018fce593ffe275eff8f94d619d9012dabde2978
GIles Roadnight <giles.roadnight@gmail.com>
2021-02-15 13:19:06 +0000

    Add version to title

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { version } from '../../package.json';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './components/app/app.component';
 import { HeartRateComponent } from './components/heart-rate/heart-rate.component';
@@ -13,4 +14,9 @@ import { HeartRateDevice } from './devices/heart-rate.device';
     providers: [HeartRateDevice],
     bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+    constructor() {
+        console.log(`setting version: ${version}`);
+        document.title = `HRM Trainer Control ${version}`;
+    }
+}
