de04750487b6f082eb763abb43ba6432fd2cdedc
GIles Roadnight <giles.roadnight@gmail.com>
2021-02-16 12:08:59 +0000

    Add logging code

--- src/app/devices/trainer.device.ts
+++ src/app/devices/trainer.device.ts
@@ -69,6 +69,53 @@ export class TrainerDevice {
             this.server = server;
 
             const updatesStream: Observable<TrainerResult | ProgressMessage> = connectServer(server).pipe(
+                tap((server) => {
+                    if (isProgressMessage(server)) {
+                        return;
+                    }
+
+                    console.log(`TrainerDevice.connect: server connected`);
+
+                    function handleCharacteristic(characteristic: BluetoothRemoteGATTCharacteristic) {
+                        console.log(`characteristic`, characteristic);
+
+                        console.log(`characteristic.getDescriptors`);
+                        characteristic.getDescriptors().then(
+                            (descriptors) => console.log(`descriptors`, descriptors),
+                            (err) => {
+                                console.log(`ERROR: characteristic.getDescriptors`, err);
+                            },
+                        );
+                    }
+
+                    function handleService(service: BluetoothRemoteGATTService) {
+                        console.log(`service`, service);
+
+                        try {
+                            console.log(`service.getIncludedServices()`);
+                            service.getIncludedServices().then((services) => services.forEach(handleService));
+                        } catch (err) {
+                            console.log(`Error getIncludedServices`, err);
+                        }
+                        try {
+                            console.log(`service.getCharacteristics()`);
+                            service
+                                .getCharacteristics()
+                                .then((characteristics) => characteristics.forEach(handleCharacteristic));
+                        } catch (err) {
+                            console.log(`Error getCharacteristics`, err);
+                        }
+                    }
+
+                    console.log(`server.getPrimaryServices()`);
+                    server.getPrimaryServices().then(
+                        (services) => services.forEach(handleService),
+                        (error) => {
+                            console.log(`Error getting service`, error);
+                        },
+                    );
+                }),
+
                 switchMap((v) => (isProgressMessage(v) ? of(v) : getService(v, 'fitness_machine'))),
                 switchMap((v) => (isProgressMessage(v) ? of(v) : v.getCharacteristic('indoor_bike_data'))),
                 tap((v) => {
